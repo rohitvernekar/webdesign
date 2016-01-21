e6f3ad4dd32f4d80a64e12c8941f419313bbdd30
Rohit Vernekar <rohitvernekar007@gmail.com>
2016-01-21 23:49:07 +0530

    Added index url

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url('^$', views.index ,name='index'),
+        #url(r'^index', views.index , name='index'),
         url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
         url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
         url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
--- webdesign/urls.py
+++ webdesign/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+import polls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
+    url(r'^index/', polls.views.index),
 ]
